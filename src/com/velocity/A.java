0cbc36233691545abad8f9e707c5556a8cbac79a
madhuri <madhuriprakhe@gmail.com>
2023-01-19 16:26:02 +0530

    demo project

---
+++ src/com/velocity/A.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class A {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		al.add(100);
+		al.add(200);
+		al.add(300);
+		System.out.println(al);
+
+	}
+
+}
